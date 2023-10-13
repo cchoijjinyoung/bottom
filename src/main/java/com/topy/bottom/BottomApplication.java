1c2c02dd7526aca5113b37ad30e3fed59f2f42ba
cchoijjinyoung <cgy7127@naver.com>
2023-10-13 21:54:50 +0900

    First push

---
+++ src/main/java/com/topy/bottom/BottomApplication.java
@@ -0,0 +1,13 @@
+package com.topy.bottom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BottomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BottomApplication.class, args);
+	}
+
+}
